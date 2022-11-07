3c07e5a7cf102012929c7008ba713fb5c4d9f68f
Glennificent <117614879+Glennificent@users.noreply.github.com>
2022-11-07 18:34:15 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -13,24 +13,23 @@ def multiply(a,b):
 def divide (a,b):
     total = a / b
     return total
-while True:
-    print("Select an Operation")
-    print("1. Add")
-    print("2. Subtract")
-    print("3. Multiply")
-    print("4. Divide")
-    selection = input("Select an operation 1-4 ")
-    a = int(input("Enter first number "))
-    b = int(input("Enter second number "))
-    if selection == '1':
-        print(a," + ", b, " = ", add(a,b))
-    elif selection == '2':
-        print(a, " - ", b, " = ", subtract(a,b))
-    elif selection == '3':
-        print(a," * ", b ," = ", multiply(a,b))
-    elif selection == '4':
-        print(a ," / ", b ," = ", round(divide(a,b), 2))
-    else:
-        print("Invalid selection")
+print("Select an Operation")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+selection = input("Select an operation 1-4 ")
+a = int(input("Enter first number "))
+b = int(input("Enter second number "))
+if selection == '1':
+    print(a," + ", b, " = ", add(a,b))
+elif selection == '2':
+    print(a, " - ", b, " = ", subtract(a,b))
+elif selection == '3':
+    print(a," * ", b ," = ", multiply(a,b))
+elif selection == '4':
+    print(a ," / ", b ," = ", round(divide(a,b), 2))
+else:
+    print("Invalid selection")
 
   
